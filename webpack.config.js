a20a4db81d3573f22da57fe285c9bb97b38fba01
Vyacheslav Levyy <vyacheslav.levyy@gmail.com>
2022-06-21 21:45:39 +0300

    Add HtmlWebpackPlugin and dist folder auto-cleaning before building

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'production',
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     module: {
         rules: [
