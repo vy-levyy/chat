5fc06f04cdba2f3c30915cf87067ce75f4f956e0
Vyacheslav Levyy <vyacheslav.levyy@gmail.com>
2022-06-21 21:11:57 +0300

    Add styles, images and fonts webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,23 @@ module.exports = {
     mode: 'production',
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
     },
 };
