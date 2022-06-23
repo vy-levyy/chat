e7c9d7ca38737a7541cfce99ed4f251133b3583a
Vyacheslav Levyy <vyacheslav.levyy@gmail.com>
2022-06-23 19:45:20 +0300

    Add typescript

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: {
-        index: './src/index.js',
+        index: './src/index.ts',
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -34,7 +34,20 @@ module.exports = {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: 'asset/resource',
             },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
         ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            "@ui": path.resolve('./src/ui/'),
+            "@components": path.resolve('./src/components/'),
+            "@pages": path.resolve('./src/pages/'),
+        }
     },
     optimization: {
         runtimeChunk: 'single',
